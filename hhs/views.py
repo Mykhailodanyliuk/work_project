236119055f5f177898c0e3647507ab58aba70144
Mykhailo Danyliuk <mihailodaniliuk@gmail.com>
2023-02-11 16:17:26 +0200

    v 1.4.1.2.0

--- hhs/views.py
+++ hhs/views.py
@@ -27,7 +27,9 @@ def display_hhs_data_individual(request):
     counter_data = update_collection.find_one({'name': 'hhs_individuals'})
     collection_count_documents = counter_data.get('total_records') if counter_data else 1
     paginator = mongo_paginator(request, collection_count_documents, 500)
-    part_npi = nppes_data_individual_collection.find().skip(
+    part_npi = nppes_data_individual_collection.find({},
+                                                     {'_id': 0, 'npi': 1, 'data.provider_first_name': 1,
+                                                      'data.provider_last_name__legal_name_': 1, 'upload_at': 1}).skip(
         500 * (int(request.GET.get('page', 1)) - 1)).limit(500)
     return render(request, 'hhs/nppes_individual.html',
                   context={'dataset': part_npi, 'paginator': paginator, 'counter_data': counter_data})
@@ -39,7 +41,9 @@ def display_hhs_data_entities(request):
     counter_data = update_collection.find_one({'name': 'hhs_entities'})
     collection_count_documents = counter_data.get('total_records') if counter_data else 1
     paginator = mongo_paginator(request, collection_count_documents, 500)
-    part_npi = nppes_data_entities_collection.find().skip(
+    part_npi = nppes_data_entities_collection.find({}, {'_id': 0, 'npi': 1,
+                                                        'data.provider_organization_name__legal_business_name_ ': 1,
+                                                        'upload_at': 1}).skip(
         500 * (int(request.GET.get('page', 1)) - 1)).limit(500)
     return render(request, 'hhs/nppes_entities.html',
                   context={'dataset': part_npi, 'paginator': paginator, 'counter_data': counter_data})
